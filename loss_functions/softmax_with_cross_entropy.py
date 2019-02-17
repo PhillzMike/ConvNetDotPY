6317b4082f7125849a32790bfe3089aa3715d857
Timilehin <phillzmike@gmail.com>
2019-02-17 22:58:34 +0100

    Added SoftmaxCrossEntropy
    Signed-off-by: Timilehin <phillzmike@gmail.com>

---
+++ loss_functions/softmax_with_cross_entropy.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+class SoftmaxCrossEntropy:
+    def __init__(self):
+        self.prob = np.NaN
+        self.__trained = False
+
+    def forward_pass(self, logits, labels):
+        assert logits.shape[0] == labels.shape[0], "logits and label must contain the same number of objects in a batch"
+        batch_size = logits.shape[0]
+        exp_logits = np.exp(logits)
+        #   TODO figure a better way to do this guy
+        self.prob = exp_logits / np.sum(exp_logits, axis=1, keepdims=True)
+        loss_for_each_image = -np.log(self.prob[range(batch_size), labels])
+        loss = np.sum(loss_for_each_image) / batch_size
+        #   free up space
+        del exp_logits, loss_for_each_image
+        self.__trained = True
+        return loss
+
+    def backward_pass(self, labels):
+        assert self.__trained, "backward pass cannot be called before forward pass"
+        batch_size = labels.shape[0]
+        d_logits = self.prob
+        d_logits[range(batch_size), labels] -= 1
+        d_logits /= batch_size
+        return d_logits
--- nn/ConvNet.py
+++ nn/ConvNet.py
@@ -11,52 +11,16 @@ from builtins import range
 from timeit import default_timer as timer
 
 import numpy as np
-from Mode import Mode
+from mode.Mode import Mode
 
 
 class Cnn:
-    def __init__(self, feature_layers, classifier_layers, loss_function, optimizer, num_of_labels):
-        #   First Convolution Layer
-        # conv_p = 0.8
-        # fstride_for_conv = 1
-        # self.firstConv = Conv((32, 32, 3), (5, 5), 20, stride=fstride_for_conv)
-        # self.firstRelu = Relu((28, 28, 20))
-        # self.firstDropout = Dropout((28, 28, 20), conv_p)
-        # self.firstMaxPool = MaxPool((28, 28, 20), 2, 2)
-        #
-        # #   Second Convolution Layer
-        # sstride_for_conv = 1
-        # self.secondConv = Conv((14, 14, 20), (3, 3), 50, stride=sstride_for_conv)
-        # self.secondRelu = Relu((12, 12, 50))
-        # self.secondDropout = Dropout((12, 12, 50), conv_p)
-        # self.secondMaxPool = MaxPool((12, 12, 50), 2, 2)
-        #
-        # #   first fully connected layer
-        # self.firstFc = FC(1800, 500)
-        # self.firstFcRelu = Relu(500)
-        #
-        # #   Batch Normallization Layer
-        # self.batchNorm = BatchNorm(500)
-        #
-        # #   Dropout layer
-        # p = 0.6
-        # self.dropout = Dropout(500, p)
-        #
-        # #   second fully connected layer
-        # self.secondFc = FC(500, num_of_labels)
-
-        # self.feature_extractor_layers = [self.firstConv, self.firstRelu, self.firstDropout, self.firstMaxPool,
-        #                                  self.secondConv, self.secondRelu, self.secondDropout,  self.secondMaxPool]
-        #
-        # self.classifier_layers = [self.firstFc, self.batchNorm, self.firstFcRelu,
-        #                           self.dropout, self.secondFc]
+    def __init__(self, feature_layers, classifier_layers, loss_function, optimizer):
         self.feature_extractor_layers = feature_layers
         self.classifier_layers = classifier_layers
 
         self.layers = self.feature_extractor_layers + self.classifier_layers
         self.loss_function = loss_function
-        # self.layers = [self.firstConv, self.firstMaxPool, self.secondConv, self.secondMaxPool, self.firstFc,
-        #                self.dropout, self.secondFc]
 
         self._optimizer = optimizer
         self.optimizers = {layer: {param: copy.deepcopy(self._optimizer) for param in layer.trainable_parameters()}
@@ -113,13 +77,12 @@ class Cnn:
         return feature_extractor_grad_output
 
     def train(self, data, label, valid_data, valid_label, batch, no_of_epochs):
-        assert data.shape[0] == label.shape[0], " The traning data and training label must be the same number"
-        assert valid_data.shape[0] == valid_label.shape[0], " The validation data and validation labe mustb be the" \
+        assert data.shape[0] == label.shape[0], " The training data and training label must be the same number"
+        assert valid_data.shape[0] == valid_label.shape[0], " The validation data and validation label must be the" \
                                                             "same number"
-        # hyperparameters
+        # hyper-parameters
         step = 1e-3
         start = timer()
-        loss = 0
         running_loss = 0
         for epoch in range(no_of_epochs):
             for X, Y in self.next_batch(data, label, batch):
@@ -128,19 +91,19 @@ class Cnn:
                                                                self.classifier_layers, Mode.TRAIN)
                 #   calculate loss
                 data_loss = self.loss_function.forward_pass(logits, Y)
-                loss = data_loss
 
                 running_loss += data_loss
                 #   calculate loss gradient with respect to logits
                 dscores = self.loss_function.backward_pass(Y)
-                #   backward pass through the netowrk
+                #   backward pass through the network
                 dx = Cnn.__backward_pass(dscores, self.feature_extractor_layers, self.classifier_layers,
                                          fs_max_pool_shape)
                 del dx
-                #   update parameters based on gradients calcuated
+                #   update parameters based on gradients calculated
                 for layer in self.layers:
                     if layer in self.optimizers:
                         layer.update_params(self.optimizers[layer], step)
+
             # calculate validation loss
             if (epoch + 1) % 1 == 0:
                 valid_loss, acc = self.test(valid_data, valid_label)
