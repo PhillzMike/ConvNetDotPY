322454bcf607ab874daa12d04075b7ae59fbd9b3
Timilehin <phillzmike@gmail.com>
2019-03-18 18:37:14 +0100

    Added a list to keep track of the training loss, validation loss and validation accuracy
    Signed-off-by: Timilehin <phillzmike@gmail.com>

--- nn/NeuralNet.py
+++ nn/NeuralNet.py
@@ -15,7 +15,7 @@ import numpy as np
 from mode.Mode import Mode
 
 
-class Cnn:
+class NN:
     def __init__(self, feature_layers, classifier_layers, loss_function, optimizer):
         self.feature_extractor_layers = feature_layers
         self.classifier_layers = classifier_layers
@@ -28,16 +28,17 @@ class Cnn:
                            for layer in self.layers if len(layer.trainable_parameters()) != 0}
 
     @staticmethod
-    def next_batch(data, label, n):
+    def __next_batch(data, label, n):
         num = data.shape[0]
         arr = np.arange(num)
         np.random.shuffle(arr)
+        print("data", data.shape)
         for i in range(int(math.ceil(num / n))):
-            yield data[arr[n * i:n * (i + 1)], :, :, :], label[arr[n * i:n * (i + 1)]]
+            yield data[arr[n * i:n * (i + 1)], :], label[arr[n * i:n * (i + 1)]]
 
     def get_logits(self, data):
-        logits, fs_max_pool_shape = Cnn.__forward_pass(data, self.feature_extractor_layers,
-                                                       self.classifier_layers, Mode.TEST)
+        logits, fs_max_pool_shape = NN.__forward_pass(data, self.feature_extractor_layers,
+                                                      self.classifier_layers, Mode.TEST)
         return logits
 
     def test(self, images):
@@ -59,10 +60,10 @@ class Cnn:
     @staticmethod
     def __forward_pass(inputs, feature_extractor_layers, classifier_layers, mode):
         num_of_objects = inputs.shape[0]
-        feature_extractor_output = Cnn.__forward(inputs, feature_extractor_layers, mode)
+        feature_extractor_output = NN.__forward(inputs, feature_extractor_layers, mode)
         feature_extractor_output_shape = feature_extractor_output.shape
         feature_extractor_output.shape = (num_of_objects, -1)
-        classifier_scores = Cnn.__forward(feature_extractor_output, classifier_layers, mode)
+        classifier_scores = NN.__forward(feature_extractor_output, classifier_layers, mode)
         return classifier_scores, feature_extractor_output_shape
 
     @staticmethod
@@ -75,9 +76,9 @@ class Cnn:
 
     @staticmethod
     def __backward_pass(d_logits, feature_extractor_layers, classifier_layers, feature_extractor_output_shape):
-        classifier_grad_output = Cnn.__backward(d_logits, classifier_layers)
+        classifier_grad_output = NN.__backward(d_logits, classifier_layers)
         classifier_grad_output.shape = feature_extractor_output_shape
-        feature_extractor_grad_output = Cnn.__backward(classifier_grad_output, feature_extractor_layers)
+        feature_extractor_grad_output = NN.__backward(classifier_grad_output, feature_extractor_layers)
         return feature_extractor_grad_output
 
     def train(self, data, label, valid_data, valid_label, batch, no_of_epochs, learning_rate, print_every):
@@ -87,37 +88,42 @@ class Cnn:
         # hyper-parameters
         start = timer()
         running_loss = 0
-        for epoch in range(no_of_epochs):
-            for X, Y in self.next_batch(data, label, batch):
+        training_loss = list()
+        validation_loss = list()
+        validation_accuracy = list()
+        for epoch in range(1, no_of_epochs + 1):
+            for X, Y in NN.__next_batch(data, label, batch):
                 #   forward pass into the network
-                logits, fs_max_pool_shape = Cnn.__forward_pass(X, self.feature_extractor_layers,
-                                                               self.classifier_layers, Mode.TRAIN)
+                logits, fs_max_pool_shape = NN.__forward_pass(X, self.feature_extractor_layers,
+                                                              self.classifier_layers, Mode.TRAIN)
                 #   calculate loss
                 data_loss = self.loss_function.forward_pass(logits, Y)
 
                 running_loss += data_loss
                 #   calculate loss gradient with respect to logits
-                dscores = self.loss_function.backward_pass(Y)
+                d_scores = self.loss_function.backward_pass(Y)
                 #   backward pass through the network
-                dx = Cnn.__backward_pass(dscores, self.feature_extractor_layers, self.classifier_layers,
-                                         fs_max_pool_shape)
+                dx = NN.__backward_pass(d_scores, self.feature_extractor_layers, self.classifier_layers,
+                                        fs_max_pool_shape)
                 del dx
                 #   update parameters based on gradients calculated
                 for layer in self.layers:
                     if layer in self.optimizers:
                         layer.update_params(self.optimizers[layer], learning_rate)
-
+            training_loss.append(running_loss / (data.shape[0] / batch))
             # calculate validation loss
-            if (epoch + 1) % print_every == 0:
-                logits, label = self.test(valid_data)
-                valid_loss = self.loss_function.forward_pass(logits, valid_label)
-                acc = np.mean(label == valid_label) * 100
+            logits, label_gotten = self.test(valid_data)
+            valid_loss = self.loss_function.forward_pass(logits, valid_label)
+            acc = np.mean(label_gotten == valid_label) * 100
+            validation_loss.append(valid_loss)
+            validation_accuracy.append(acc)
+            if epoch % print_every == 0:
                 print("The validation loss is ", valid_loss, " Accuracy: ", acc)
                 print("The loss after ", epoch, " iterations, learning rate is", learning_rate, "iterations is ",
-                      running_loss / (data.shape[0] / batch), " using ",
-                      timer() - start)
+                      training_loss[-1], " using ", timer() - start)
 
             running_loss = 0
+        return training_loss, validation_loss, validation_accuracy
 
     def save(self, filename):
         with open(filename, 'wb') as output:  # Overwrites any existing file.
