b440fb128d8ed75f80c45359fd287bb2399b0a60
Timilehin <phillzmike@gmail.com>
2022-12-26 16:14:45 +0000

    Make the nodes after dropout scale by 1/1-p

--- layers/Dropout.py
+++ layers/Dropout.py
@@ -12,16 +12,16 @@ from mode.Mode import Mode
 
 class Dropout(Layer):
 
-    def __init__(self, inp_shape, p):
+    def __init__(self, inp_shape, p=0.5):
         super(Dropout, self).__init__(inp_shape)
-        self.probability = np.float32(p)
+        self._p = np.float32(p)
         self._drop_prob = np.float32(1)
 
     def forward_pass(self, inp):
         self._inp = inp
         self._drop_prob = np.float32(1)
         if self.mode == Mode.TRAIN:
-            self._drop_prob = ((np.random.rand(*self._inp.shape, dtype=np.float32) < self.probability) / self.probability)
+            self._drop_prob = ((np.random.rand(*self._inp.shape, dtype=np.float32) < self._p) / (1 - self._p))
             # self._drop_prob = ((np.float32(np.random.rand(*self._inp.shape)) < self.probability) / self.probability)
         result = self._inp * self._drop_prob
         return result
